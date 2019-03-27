008c931070888ee8c551d1cc8b708f56daa4c9a9
Beheroth <arnaud.peeters@hotmail.com>
2019-03-27 15:55:14 +0100

    Initial commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name);
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+        }
+        // Destroy everything that leaves the trigger
+        Destroy(other.gameObject);  //NB: marks object to be destroyed at the end of the frame
+        Destroy(gameObject);
+    }
+}
